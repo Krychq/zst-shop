98971ead1d9761377d29e84c571af76152072261
Krystian <krystianurban100100@gmail.com>
2023-11-19 16:56:02 -0100

    Add ReactQuery and products json

---
+++ src/components/providers/react-query-provider.tsx
@@ -0,0 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+interface ReactQueryProviderProps {
+  children: React.ReactNode;
+}
+
+export const ReactQueryProvider: React.FC<ReactQueryProviderProps> = ({
+  children,
+}) => {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
